13857bc0fdc7911c698075f5b145283159d64c89
fdgongora2 <fdgongora@iesmurgi.org>
2021-11-03 18:41:04 +0100

    Configuración de la librería JDBC para Mariadb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.example.javafx_gestion_bbdd_tarea_2_3 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.mariadb.jdbc;
+    requires java.sql;
 
 
     opens com.example.javafx_gestion_bbdd_tarea_2_3 to javafx.fxml;
