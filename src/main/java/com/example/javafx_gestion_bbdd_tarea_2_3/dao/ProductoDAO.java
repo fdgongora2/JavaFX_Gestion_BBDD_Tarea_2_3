c720ec9e2649181c69ea43ab86491912cd5f7b14
fdgongora2 <fdgongora@iesmurgi.org>
2021-11-05 07:56:40 +0100

    Implementación del modelo Producto y en el DAO, la carga de todos los productos (Sin probar)

---
+++ src/main/java/com/example/javafx_gestion_bbdd_tarea_2_3/dao/ProductoDAO.java
@@ -0,0 +1,61 @@
+package com.example.javafx_gestion_bbdd_tarea_2_3.dao;
+
+import com.example.javafx_gestion_bbdd_tarea_2_3.modelos.Producto;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductoDAO {
+
+    private final String servidor = "jdbc:mariadb://localhost:5555/noinch?useSSL=false";
+    private final String usuario = "adminer";
+    private final String passwd ="adminer";
+
+    private Connection conexionBBDD;
+
+    public ObservableList<Producto> obtenerProductos () throws SQLException {
+
+        ObservableList<Producto> datosResultadoConsulta = FXCollections.observableArrayList();
+        try {
+            // Nos conectamos
+            conexionBBDD = DriverManager.getConnection(servidor, usuario, passwd);
+            String SQL = "SELECT * "
+                    + "FROM products ";
+
+            // Ejecutamos la consulta y nos devuelve una matriz de filas (registros) y columnas (datos)
+            ResultSet resultadoConsulta = conexionBBDD.createStatement().executeQuery(SQL);
+
+            // Debemos cargar los datos en el ObservableList (Que es un ArrayList especial)
+            // Los datos que devuelve la consulta no son directamente cargables en nuestro objeto
+            while (resultadoConsulta.next()) {
+                datosResultadoConsulta.add(new Producto(
+                        resultadoConsulta.getString("productCode"),
+                        resultadoConsulta.getString("productName"),
+                        resultadoConsulta.getString("productLine"),
+                        resultadoConsulta.getString("productScale"),
+                        resultadoConsulta.getString("productVendor"),
+                        resultadoConsulta.getString("productDescription"),
+                        resultadoConsulta.getInt("quantityInStock"),
+                        resultadoConsulta.getDouble("buyPrice"),
+                        resultadoConsulta.getDouble("MSRP"))
+                );
+                System.out.println("Row [1] added " + resultadoConsulta.toString());
+            }
+            conexionBBDD.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error:" + e.toString());
+            conexionBBDD.close();
+        } finally {
+            return datosResultadoConsulta;
+        }
+    }
+
+}
---
+++ src/main/java/com/example/javafx_gestion_bbdd_tarea_2_3/modelos/Producto.java
@@ -0,0 +1,153 @@
+package com.example.javafx_gestion_bbdd_tarea_2_3.modelos;
+
+import javafx.beans.property.*;
+
+public class Producto {
+
+  private StringProperty productCode;
+  private StringProperty productName;
+  private StringProperty productLine;
+  private StringProperty productScale;
+  private StringProperty productVendor;
+  private StringProperty productDescription;
+  private IntegerProperty quantityInStock;
+  private DoubleProperty buyPrice;
+  private DoubleProperty MSRP;
+
+  public Producto(String productCode, String productName, String productLine, String productScale,
+                  String productVendor, String productDescription, Integer quantityInStock, Double buyPrice,
+                  Double MSRP) {
+    this.productCode = new SimpleStringProperty(productCode);
+    this.productName = new SimpleStringProperty(productName);
+    this.productLine = new SimpleStringProperty(productLine);
+    this.productScale = new SimpleStringProperty(productScale);
+    this.productVendor = new SimpleStringProperty(productVendor);
+    this.productDescription = new SimpleStringProperty( productDescription);
+    this.quantityInStock = new SimpleIntegerProperty(quantityInStock);
+    this.buyPrice = new SimpleDoubleProperty(buyPrice);
+    this.MSRP = new SimpleDoubleProperty(MSRP);
+  }
+
+  @Override
+  public String toString() {
+    return "Producto{" +
+            "productCode=" + productCode +
+            ", productName=" + productName +
+            ", productLine=" + productLine +
+            ", productScale=" + productScale +
+            ", productVendor=" + productVendor +
+            ", productDescription=" + productDescription +
+            ", quantityInStock=" + quantityInStock +
+            ", buyPrice=" + buyPrice +
+            ", MSRP=" + MSRP +
+            '}';
+  }
+
+  public String getProductCode() {
+    return productCode.get();
+  }
+
+  public StringProperty productCodeProperty() {
+    return productCode;
+  }
+
+  public void setProductCode(String productCode) {
+    this.productCode.set(productCode);
+  }
+
+  public String getProductName() {
+    return productName.get();
+  }
+
+  public StringProperty productNameProperty() {
+    return productName;
+  }
+
+  public void setProductName(String productName) {
+    this.productName.set(productName);
+  }
+
+  public String getProductLine() {
+    return productLine.get();
+  }
+
+  public StringProperty productLineProperty() {
+    return productLine;
+  }
+
+  public void setProductLine(String productLine) {
+    this.productLine.set(productLine);
+  }
+
+  public String getProductScale() {
+    return productScale.get();
+  }
+
+  public StringProperty productScaleProperty() {
+    return productScale;
+  }
+
+  public void setProductScale(String productScale) {
+    this.productScale.set(productScale);
+  }
+
+  public String getProductVendor() {
+    return productVendor.get();
+  }
+
+  public StringProperty productVendorProperty() {
+    return productVendor;
+  }
+
+  public void setProductVendor(String productVendor) {
+    this.productVendor.set(productVendor);
+  }
+
+  public String getProductDescription() {
+    return productDescription.get();
+  }
+
+  public StringProperty productDescriptionProperty() {
+    return productDescription;
+  }
+
+  public void setProductDescription(String productDescription) {
+    this.productDescription.set(productDescription);
+  }
+
+  public int getQuantityInStock() {
+    return quantityInStock.get();
+  }
+
+  public IntegerProperty quantityInStockProperty() {
+    return quantityInStock;
+  }
+
+  public void setQuantityInStock(int quantityInStock) {
+    this.quantityInStock.set(quantityInStock);
+  }
+
+  public double getBuyPrice() {
+    return buyPrice.get();
+  }
+
+  public DoubleProperty buyPriceProperty() {
+    return buyPrice;
+  }
+
+  public void setBuyPrice(double buyPrice) {
+    this.buyPrice.set(buyPrice);
+  }
+
+  public double getMSRP() {
+    return MSRP.get();
+  }
+
+  public DoubleProperty MSRPProperty() {
+    return MSRP;
+  }
+
+  public void setMSRP(double MSRP) {
+    this.MSRP.set(MSRP);
+  }
+}
